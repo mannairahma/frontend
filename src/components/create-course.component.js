713cd47cee231a5ceda4b529ace7c5833043a183
mannai <mannai@isikef.u-jendouba.tn>
2023-01-02 12:53:36 +0100

    update component course

--- src/components/create-course.component.js
+++ src/components/create-course.component.js
@@ -1,13 +1,9 @@
-import React, { Component } from "react";
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+import React from 'react'
 
-export default class CreateCourse extends Component {
-
-  render() {
-    return (<div className="form-wrapper">
-        <h1>Create Course</h1>     
-          </div>);
-  }
+class CreateCourse extends React.Component {
+    render() {
+        return <h1>Create Course</h1>
+    }
 }
+
+export default CreateCourse
